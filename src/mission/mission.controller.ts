bb1047ccf9e5d236d2a43376490ced2c3d44fe98
izcazy <ice.izcazy@gmail.com>
2026-03-23 22:02:25 +0700

    p02

--- src/mission/mission.controller.ts
+++ src/mission/mission.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { MissionService } from './mission.service';
+import { IMission } from './mission.interface';
 
 @Controller('missions')
 export class MissionController {
   constructor(private readonly missionService: MissionService) {}
-
+  //p01
   @Get('summary')
   getSummary() {
     return this.missionService.getSummary();
+  }
+  @Get()
+  getMissions(): IMission[] {
+    return this.missionService.findAll();
   }
 }
--- src/mission/mission.service.ts
+++ src/mission/mission.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { IMission } from './mission.interface';
+import process from 'process';
+import path from 'path';
+import * as fs from 'fs';
 
 @Injectable()
+//p01
 export class MissionService {
   private readonly missions = [
     { id: 1, codename: 'OPERATION_STORM', status: 'ACTIVE' },
@@ -30,5 +35,32 @@ export class MissionService {
       COMPLETED: completed,
       FAILED: failed,
     };
+  }
+  //p02
+  findAll(): IMission[] {
+    const filePath = path.join(process.cwd(), 'data', 'missions.json');
+    const fileData = fs.readFileSync(filePath, 'utf-8');
+    // ใช้ as IMission[] เพื่อเลี่ยง Unsafe assignment error
+    const missions: IMission[] = JSON.parse(fileData) as IMission[];
+
+    return missions.map((mission) => {
+      // เงื่อนไข: ถ้า endDate เป็น null ให้ส่ง -1
+      if (!mission.endDate) {
+        return { ...mission, durationDays: -1 };
+      }
+
+      // คำนวณระยะเวลา
+      const start = new Date(mission.startDate as string);
+      const end = new Date(mission.endDate);
+      
+      // สูตร: (ปลายทาง - ต้นทาง) / เวลา 1 วันในหน่วย ms
+      const diffTime = end.getTime() - start.getTime();
+      const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+      return {
+        ...mission,
+        durationDays: diffDays,
+      };
+    });
   }
 }
